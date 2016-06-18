6dcbf6cc4b20a3442612fb59ea1cc40a1628a372
Mari Johannessen <mari.l.johannessen@gmail.com>
2016-06-18 15:00:02 -0500

    Cleaned up

--- tools/build.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const task = require('./task');
-
-module.exports = task('build', () => Promise.resolve()
-  .then(() => require('./clean'))
-  .then(() => require('./copy'))
-  .then(() => require('./bundle'))
-);
--- tools/clean.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-// 
-// const del = require('del');
-// const task = require('./task');
-//
-// module.exports = task('clean', () => del(['build/*', '!build/.git'], { dot: true }));
